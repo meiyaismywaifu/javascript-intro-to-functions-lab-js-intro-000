91d6e7e90d7663698d3f759ab6d0f9f64249e78f
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-21 22:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function logShout(string){
   console.log(string.toUpperCase);
 }
 
-function longWhisper(string){
+function logWhisper(string){
   var lc = string.toLowerCase;
   console.log(lc);
 }
