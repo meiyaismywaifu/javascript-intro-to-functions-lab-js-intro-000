228afc8847d19b0e458143cbc62b93e7ac20ff8c
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-21 21:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function whisper(string){
 }
 
 function logShout(string){
-
+  console.log(string.toUpperCase);
 }
 
 function longWhisper(string){
-
+  console.log(string.toLowerCase);
 }
 
 function sayHiToGrandma(string){
